4219c80f0f3c26c5d343e10c2a48fc20f70af094
Sebők Tamás <tridasz@gmail.com>
2017-10-23 17:38:35 +0200

    setup application

---
+++ book-catalogue/src/main/java/com/example/bookcatalogue/BookCatalogueApplication.java
@@ -0,0 +1,12 @@
+package com.example.bookcatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCatalogueApplication.class, args);
+	}
+}
